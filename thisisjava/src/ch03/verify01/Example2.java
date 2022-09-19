18d8a77ef46bd1c88b6f6f151c7988b126699f0b
admin <admin@PC1>
2022-09-19 15:21:42 +0900

    1

--- thisisjava/src/ch03/verify01/Example2.java
+++ thisisjava/src/ch03/verify01/Example2.java
@@ -3,8 +3,8 @@ package ch03.verify01;
 public class Example2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num = (int)(Math.random()*6)+1;
+		System.out.println(num);
 	}
 
 }

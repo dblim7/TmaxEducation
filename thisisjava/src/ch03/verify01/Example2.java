4447a59fb0cf8d767b83e15d3c1430560645d986
admin <admin@PC1>
2022-09-19 14:39:20 +0900

    1

---
+++ thisisjava/src/ch03/verify01/Example2.java
@@ -0,0 +1,10 @@
+package ch03.verify01;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ thisisjava/src/ch03/verify01/example.java
@@ -0,0 +1,13 @@
+package ch03.verify01;
+
+public class example {
+
+	public static void main(String[] args) {
+	int pencils = 534;
+	int students = 30;
+	
+	//학생 한 명이 가지는 연필 수
+	//남은 연필 수
+	}
+
+}
